af246108e2a98da87e1b00148f7f7858d1df924a
Masoud <masoud.fatemi1990@gmail.com>
2024-02-13 17:27:42 +0200

    update

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ Created on Tue Feb 13 17:11:36 2024
 related to "Kimmo-Ilari Juntunen", a studet who asked for data (check uef email for more info)
 
 """
+import os
 
+input_dir = 'data/'
+
+#to get all the json files in a directory
+json_files = [file for file in os.listdir(input_dir) if file.endswith('.json')]

926d10d88dcd3a43891ed4888407f8f47ba49ffd
Masoud <masoud.fatemi1990@gmail.com>
2024-02-13 17:13:58 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 17:11:36 2024
+
+@author: Masou
+
+related to "Kimmo-Ilari Juntunen", a studet who asked for data (check uef email for more info)
+
+"""
+
